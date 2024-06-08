2811ea1eea4866293aafb07791a1abfcaaab2f81
Luís Fernando <nando.1regis@gmail.com>
2024-06-08 17:23:58 -0300

    Server node

--- index.js
+++ index.js
@@ -3,4 +3,3 @@ import Viewer from "./classes/viewer.js";
 const main = document.querySelector('.modelo');
 
 const view = new Viewer( main , './models/manequim2.glb');
-// view.add('./models/manequim2.glb');
---
+++ server.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const port = 3000;
+
+const mimeTypes = {
+  '.html': 'text/html',
+  '.js': 'application/javascript',
+  '.css': 'text/css',
+  '.json': 'application/json',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.gif': 'image/gif',
+  '.wav': 'audio/wav',
+  '.mp4': 'video/mp4',
+  '.woff': 'application/font-woff',
+  '.ttf': 'application/font-ttf',
+  '.eot': 'application/vnd.ms-fontobject',
+  '.otf': 'application/font-otf',
+  '.svg': 'application/image/svg+xml',
+  '.hdr': 'application/octet-stream',
+  '.gltf': 'model/gltf+json',
+  '.bin': 'application/octet-stream'
+};
+
+const server = http.createServer((req, res) => {
+
+//   console.log(`Request URL: ${req.url}`);
+
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  let extname = String(path.extname(filePath)).toLowerCase();
+  let contentType = mimeTypes[extname] || 'application/octet-stream';
+
+  fs.readFile(filePath, (error, content) => {
+    if (error) {
+      if (error.code === 'ENOENT') {
+        fs.readFile(path.join(__dirname, 'public', '404.html'), (error, content) => {
+          res.writeHead(404, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        });
+      } else {
+        res.writeHead(500);
+        res.end(`Sorry, check with the site admin for error: ${error.code} ..\n`);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
